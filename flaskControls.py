ef8e4ff6f08bd01ba87105cea41a4c745b08bd27
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2022-08-07 20:33:32 +0200

    Update flaskControls.py

--- flaskControls.py
+++ flaskControls.py
@@ -5,6 +5,7 @@ from flask import Flask, redirect, url_for, render_template
 from StepperClass import StepperControls
 
 app = Flask(__name__)
+gpio.setmode(gpio.BOARD)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
