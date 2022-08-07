1a3ac9367d0783b19a1008e29198a82e02bd675a
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2022-08-07 18:16:04 +0200

    Add files via upload

---
+++ StepperClass.py
@@ -0,0 +1,46 @@
+class StepperControls:
+
+    def __init__(self, pins, direction):
+        self.pins = pins
+        self.direction = direction
+        # create global variable for the fullstep sequence
+        global seq
+        seq = [
+            [1, 1, 0, 0], 
+            [0, 1, 1, 0], 
+            [0, 0, 1, 1], 
+            [1, 0, 0, 1] 
+            ]
+
+    def setup(self):
+        gpio.setmode(gpio.BOARD)
+
+        # setup output pins and set output to 0
+        for pin in self.self.pins:
+            gpio.setup(pin, gpio.OUT)
+            gpio.output(pin, 0)
+
+    def forward():
+        print("Moving forward...")
+        # Move forward
+        while 1:
+            for fullstep in range(4):
+                for pin in range(4):
+                    gpio.output(self.pins[pin], seq[fullstep][pin])
+                    time.sleep(0.001)
+
+    def back():
+        print("Moving backwards...")
+        # reverse the direction of previous function
+        time.sleep(0.001)
+        for fullstep in range(4):
+            for pin in range(4):
+                # inverse to pins array to iterate through the array backwards; making it go reverse
+                gpio.output(self.pins[::-1][pin], seq[fullstep][pin])
+                time.sleep(0.001)
+    
+    def stop():
+        print("stopped")
+        time.sleep(0.001)
+        gpio.cleanup()
+                
---
+++ flaskControls.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as gpio
+import time
+
+from flask import Flask, redirect, url_for, render_template
+from StepperClass import StepperControls
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    test = StepperControls([7, 11, 13, 15], "S")
+    test.stop()
+    return render_template("index.html")
+
+@app.route("/F")
+def forward():
+    test = StepperControls([7, 11, 13, 15], "F")
+    test.forward()
+    return render_template("forward.html")
+
+@app.route("/B")
+def back():
+    test = StepperControls([7, 11, 13, 15], "B")
+    test.back()
+    return render_template("backward.html")
+
+@app.route("/L")
+def left():
+    return render_template("left.html")
+
+@app.route("/R")
+def right():
+    return render_template("right.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''
+# {{url}}/led?status=on
+@app.route('/', methods=['GET'])
+def led():
+    status = request.args.get('status')
+    if status == "on":
+        GPIO.output(18, GPIO.HIGH)
+        return jsonify({"message": "Led successfully turned on"})
+    elif status == "off":
+        GPIO.output(18, GPIO.LOW)
+        return jsonify({"message": "Led successfully turned off"})
+    else:
+        return jsonify({"message": "Not a valid status"})
+'''
