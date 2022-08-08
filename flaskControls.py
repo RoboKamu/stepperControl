a72cf39d7f307d26dc4c04de30d07285ae4c0b77
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2022-08-08 18:39:57 +0200

    Update flaskControls.py

--- flaskControls.py
+++ flaskControls.py
@@ -7,39 +7,38 @@ from StepperClass import StepperControls
 app = Flask(__name__)
 gpio.setwarnings(False)
 
-try:
-    @app.route("/", methods=["POST", "GET"])
-    def home():
-        test = StepperControls([7, 11, 13, 15], "S")
-        test.stop()
-        return render_template("index.html")
+@app.route("/", methods=["POST", "GET"])
+def home():
+    test = StepperControls([7, 11, 13, 15], "S")
+    test.stop()
+    return render_template("index.html")
 
-    @app.route("/F")
-    def forward():
-        try:
-            test = StepperControls([7, 11, 13, 15], "F")
-            test.setup()
-            test.forward()
-            return render_template("forward.html")
-        finally:
-            gpio.cleanup()
-
-    @app.route("/B")
-    def back():
-        test = StepperControls([7, 11, 13, 15], "B")
+@app.route("/F")
+def forward():
+    try:
+        test = StepperControls([7, 11, 13, 15], "F")
         test.setup()
-        test.back()
-        return render_template("backward.html")
+        test.forward()
+        return render_template("forward.html")
+    finally:
+        gpio.cleanup()
 
-    @app.route("/L")
-    def left():
-        return render_template("left.html")
+@app.route("/B")
+def back():
+    
+    test = StepperControls([7, 11, 13, 15], "B")
+    test.setup()
+    test.back()
+    return render_template("backward.html")
 
-    @app.route("/R")
-    def right():
-        return render_template("right.html")
-except:
-    print("Runtime Error")
+@app.route("/L")
+def left():
+    return render_template("left.html")
+
+@app.route("/R")
+def right():
+    return render_template("right.html")
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port="5000")
