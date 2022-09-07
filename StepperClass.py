32103fd147fcec089f2e9ef75befa59786a7523f
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2022-09-07 18:36:26 +0200

    Update StepperClass.py

--- StepperClass.py
+++ StepperClass.py
@@ -36,7 +36,7 @@ class StepperControls:
                     for arr in range(2):
                         for pin in range(4):
                             gpio.output(self.pins[arr][pin], seq[fullstep][pin])
-                        time.sleep(0.00035)
+                        time.sleep(0.001)
         except:
             pass    # RuntimeError, stop() cleaned up pins so no pins availabe
         
